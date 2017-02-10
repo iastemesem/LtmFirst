53e20b4c200df48d2ef98da8edcd0c0b1684220b
Gianfranco Colucci <Gianfranco Colucci>
2017-02-10 10:29:47 +0100

    Activity_main, Activity_login & Activtiy_register

---
+++ app/src/main/java/com/iastemesem/myfirstapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.iastemesem.myfirstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Gianfranco on 08/02/2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}
